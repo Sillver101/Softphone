a98dbe702bd6069e59ae0bff1b24ff14430cf153
Darryn Lloyd <darryn.lloyd@gmail.com>
2021-02-26 16:51:24 +0200

    First buttons added, Second form added

--- Softphone/Form1.cs
+++ Softphone/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using SIPSorcery.Media;
 using SIPSorcery.SIP.App;
 using SIPSorceryMedia.Windows;
